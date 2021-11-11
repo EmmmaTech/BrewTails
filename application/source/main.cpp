b5780bd904cb51ffe913e5456f3feae47d376558
EmreTech <50607143+EmreTech@users.noreply.github.com>
2021-11-10 16:46:43 -0800

    Adding borealis

--- application/source/main.cpp
+++ application/source/main.cpp
@@ -1,29 +1,9 @@
-#include <iostream>
 #include <switch.h>
+
+#include <borealis.hpp>
+#include <string>
 
 int main(int argc, char **argv)
 {
-    consoleInit(NULL);
-
-    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
-
-    PadState pad;
-    padInitializeDefault(&pad);
-
-    std::cout << "New Homebrew Menu by EmreTech" << '\n';
-
-    while (appletMainLoop())
-    {
-        padUpdate(&pad);
-
-        u64 kdown = padGetButtonsDown(&pad);
-
-        if (kdown & HidNpadButton_Plus)
-            break;
-
-        consoleUpdate(NULL);
-    }
-
-    consoleExit(NULL);
     return 0;
 }
